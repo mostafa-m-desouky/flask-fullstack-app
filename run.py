ce324062f229ab0d3150d7332fef0176351ce6f9
mostafa-m-desouky <mostafa.m.desouky@gmail.com>
2026-03-29 16:31:42 +0200

    Delete Function Route & Use Blueprint

--- run.py
+++ run.py
@@ -1,11 +1,7 @@
-from app import create_app, db
-from app.models import Post
+from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    # First Way To Create A Table Before Use Another File (init_db.py)
-    # with app.app_context():
-    #     db.create_all()
         
     app.run(debug=True)
